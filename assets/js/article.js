8a5139d93aa9da835beba375c81190236ce004da
Marvin Blum <marvin.blum@emvi.com>
2021-07-25 14:18:02 +0200

    Added reading time script to articles.

---
+++ assets/js/article.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const script = document.getElementById("article");
+
+    if(script && script.hasAttribute("data-words") && script.hasAttribute("data-title")) {
+        const title = script.getAttribute("data-title");
+
+        if(!localStorage.getItem(title)) {
+            const readingTimeSeconds = Math.floor(parseInt(script.getAttribute("data-words"))/220)*60;
+            const start = new Date().getTime();
+            let done = false;
+            
+            window.addEventListener("scroll", () => {
+                if(!done && getScrollPercent() > 85) {
+                    const readSeconds = Math.floor((new Date().getTime()-start)/1000);
+
+                    if(readSeconds >= readingTimeSeconds) {
+                        done = true;
+                        localStorage.setItem(title, "read");
+                        pirsch("Read article", {
+                            duration: readSeconds,
+                            meta: {
+                                title
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+});
+
+// https://stackoverflow.com/a/8028584
+function getScrollPercent() {
+    const h = document.documentElement, 
+          b = document.body,
+          st = 'scrollTop',
+          sh = 'scrollHeight';
+    return Math.floor((h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100);
+}
